b32d2c415ae0bbef3132ceed6ab946805af1e720
Ben Titus <ben.titus13@gmail.com>
2016-10-31 12:52:09 +0100

    Added comments

--- lib/Stepper_Library/STEPPER.cpp
+++ lib/Stepper_Library/STEPPER.cpp
@@ -1,8 +1,14 @@
 #include "STEPPER.h"
 
+/** Default constructor
+*/
 STEPPER::STEPPER() {
 }
 
+/** Initialize the pins for the stepper motor
+    @param dirPin Pin for controlling direction
+    @param stepPin Pin for taking steps
+*/
 void STEPPER::attach(uint8_t dirPin, uint8_t stepPin) {
     this->dirPin = dirPin;
     this->stepPin = stepPin;
@@ -11,41 +17,61 @@ void STEPPER::attach(uint8_t dirPin, uint8_t stepPin) {
     dir = 0;
 }
 
-/** Takes a certain number of steps steps at a fixed frequency
-    @param steps Number of steps to take
-    @param dir Direction to rotate
+/** Takes a single step
 */
 void STEPPER::step() {
+    //Ensure the output is low
     digitalWrite(stepPin, LOW);
+    //set direction to correct direction
     digitalWrite(dirPin, dir);
+    //Toggle output
     digitalWrite(stepPin, HIGH);
-    delay(1);
+    //
     digitalWrite(stepPin, LOW);
 }
 
-/** Takes a certain number of steps steps at a fixed frequency
-    @param steps Number of steps to take
+/** Takes a single step
+    @param dir New direction value
 */
 void STEPPER::step(bool dir) {
     digitalWrite(stepPin, LOW);
+
     changeDir(dir);
+
     digitalWrite(dirPin, dir);
+
     digitalWrite(stepPin, HIGH);
-    delay(1);
+
     digitalWrite(stepPin, LOW);
 }
 
+/** Drives the stepPin output at the desired frequency in a new direction
+    @param freq Frequency from 31-65535Hz
+    @param dir New direction value
+*/
 void STEPPER::stepSpd(uint16_t freq, bool dir) {
+
     changeDir(dir);
+
     digitalWrite(dirPin, dir);
+
     tone(stepPin,freq);
 }
 
+
+/** Drives the stepPin output at the desired frequency
+    @param freq Frequency from 31-65535Hz
+*/
 void STEPPER::stepSpd(uint16_t freq) {
+    //set direction
     digitalWrite(dirPin, dir);
+    //output a square wave at the desired frequency
     tone(stepPin, freq);
 }
 
+/** Changes the direction variable
+    @param dir New direction value
+*/
 void STEPPER::changeDir(bool dir) {
     this->dir = dir;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <Arduino.h>
 #include "STEPPER.h"
+#include <Servo.h>
+
+Servo motor1;
 
 #define DIRPIN1 2
 #define STEPPIN1 3
@@ -8,6 +11,7 @@ STEPPER stepper1;
 
 void setup() {
     stepper1.attach(DIRPIN1, STEPPIN1);
+
     for (int i = 0; i < 5400; i++) {
         stepper1.step();
         delay(1);
@@ -15,4 +19,5 @@ void setup() {
 }
 
 void loop() {
+
 }
